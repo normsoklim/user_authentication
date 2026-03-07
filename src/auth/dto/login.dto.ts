64301aa722194d864415612c42815100539e0323
soklim <soklim106@gmail.com>
2026-03-07 08:17:33 +0700

    updated

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,13 +1,5 @@
-import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginDto {
-  @ApiProperty({
-    example: 'username123@gmail.com'
-  })
   email: string;
-
-  @ApiProperty({
-    example: 'password123'
-  })
   password: string;
 }
--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,37 +1,9 @@
 
-import { ApiProperty } from '@nestjs/swagger';
-
 export class RegisterDto {
-    @ApiProperty({
-        example: 'John'
-    })
     username: string;
-
-    @ApiProperty({
-        example: 'john@example.com'
-    })
     email: string;
-
-    @ApiProperty({
-        example: 'password123'
-    })
     password: string;
-
-    @ApiProperty({
-        example: 'user',
-        required: false
-    })
     role?: string;
-
-    @ApiProperty({
-        example: '123456789',
-        required: false
-    })
     googleId?: string;
-
-    @ApiProperty({
-        example: '123456789',
-        required: false
-    })
     facebookId?: string;
 }
